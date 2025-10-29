bb3866330855b27ca00ddd1b0ae82a2b63ecd3ad
sanyath007 <sanyath007@gmail.com>
2025-10-29 11:04:38 +0700

    reflactor: add new api caller as axios object

---
+++ src/api/index.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: process.env.REACT_APP_API_URL
+});
+
+api.interceptors.request.use((config) => {
+    /** Use Access Token */
+    // const token = localStorage.getItem("access_token");
+
+    // if (token) {
+    // config.headers.Authorization = `Bearer ${token}`;
+    // }
+
+    /** Use API Key */
+    const API_KEY = process.env.REACT_APP_API_KEY;
+    console.log(API_KEY);
+
+    config.headers.set('X-API-KEY', API_KEY);
+
+    return config;
+}, (error) => Promise.reject(error));
+
+api.interceptors.response.use((response) => {
+    return response;
+}, (error) => {
+    if (error.response.status === 401) {
+        return window.location.href = "/login";
+    } else {
+        return Promise.reject(error);
+    }
+})
+
+export default api;
